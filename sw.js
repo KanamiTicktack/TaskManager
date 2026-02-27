442577c0cd08256ee7c53ee175ee9c14dee90d5a
KanamiTicktack <KanamiTicktack@proton.me>
2026-02-27 13:41:11 +0900

    Rename cache and add additional resources

--- sw.js
+++ sw.js
@@ -1,24 +1,26 @@
-const CACHE_NAME = 'obsidian-tasks-v1';
+const CACHE_NAME = 'task-entry-cache-v1';
 const urlsToCache = [
   './',
   './index.html',
-  './manifest.json'
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
 ];
 
+// インストール時に指定リソースをキャッシュへ保存
 self.addEventListener('install', function(event) {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(function(cache) {
-        return cache.addAll(urlsToCache);
-      })
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(urlsToCache);
+    })
   );
 });
 
+// 通信発生時にキャッシュにデータがあればそれを返し、なければ通信する
 self.addEventListener('fetch', function(event) {
   event.respondWith(
-    caches.match(event.request)
-      .then(function(response) {
-        return response || fetch(event.request);
-      })
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
   );
 });
